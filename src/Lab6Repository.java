02b2d833f56a721a303b801ce3fa6847a86cc9ae
GabbyRochester <rochesterG@ufl.edu>
2022-03-04 00:04:30 -0500

    This is me pushing my Lab06 java file into my public repository

---
+++ src/Lab6Repository.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Lab6Repository {
+
+    public static void main(String[]args){
+        Scanner scner = new Scanner(System.in);
+        int firstNumber;
+        int secondNumber;
+
+
+        System.out.println("Enter first number: ");
+        firstNumber = scner.nextInt();
+        System.out.println("Enter second number: ");
+        secondNumber = scner.nextInt();
+
+        int result = firstNumber += secondNumber;
+
+        System.out.println(result);
+    }
+
+}
